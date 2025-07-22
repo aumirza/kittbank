44980b45fb35e76d3a3b35b6caa0c7c8b0ca19f9
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-22 15:06:21 +0530

    feat: implement routing with react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { lazy, Suspense } from 'react';
+import { Route, type RouteObject, Routes } from 'react-router';
+
+const LoginPage = lazy(() => import('./pages/Login'));
+
+const OverviewPage = lazy(() => import('./pages/Overview'));
+
+const authRoutes: RouteObject[] = [{ path: '/login', element: <LoginPage /> }];
+
+const routes: RouteObject[] = [{ path: '/', element: <OverviewPage /> }];
+
 function App() {
   return (
-    <>
-      <div className="App">
-        <h1>Welcome to My Application</h1>
-        <p>This is a simple React application.</p>
-      </div>
-    </>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {/* <Route element={<div>Layout</div>}>
+        </Route> */}
+        {authRoutes.map((route) => (
+          <Route element={route.element} key={route.path} path={route.path} />
+        ))}
+        {routes.map((route) => (
+          <Route element={route.element} key={route.path} path={route.path} />
+        ))}
+        {/* <Route path="*" element={<div>404 Not Found</div>} /> */}
+      </Routes>
+    </Suspense>
   );
 }
 
