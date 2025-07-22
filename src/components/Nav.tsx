9d05ff7d0a358575ccb1f03989450945e974f45a
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-22 20:55:15 +0530

    feat: refactor Nav component to use NavSidebarMenuButton

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,7 +12,44 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from '@/components/ui/sidebar';
+
 import { cn } from '@/lib/utils';
+
+interface NavSidebarMenuButtonProps {
+  isActive: boolean;
+  icon: React.ElementType;
+  title: string;
+}
+
+export function NavSidebarMenuButton({
+  isActive,
+  icon: Icon,
+  title,
+}: NavSidebarMenuButtonProps) {
+  return (
+    <SidebarMenuButton className="hover:bg-transparent" size="lg">
+      {isActive && (
+        <div className="absolute inset-0 bg-gradient-to-r from-white/10 to-transparent opacity-20" />
+      )}
+      <div
+        className={cn(
+          'flex size-10 items-center justify-center rounded-full bg-accent',
+          { 'bg-primary': isActive, 'bg-accent': !isActive },
+          { 'group-hover/menu-item:bg-primary': !isActive }
+        )}
+      >
+        <Icon
+          className={cn('size-5 shrink-0 transition-all duration-200', {
+            'text-white': isActive,
+          })}
+        />
+      </div>
+      <span className={cn('text-xl', { 'font-semibold': isActive })}>
+        {title}
+      </span>
+    </SidebarMenuButton>
+  );
+}
 
 const navigationItems = [
   {
@@ -52,27 +89,13 @@ export function Nav() {
     <SidebarMenu className="gap-1">
       {navigationItems.map((item) => (
         <SidebarMenuItem key={item.title}>
-          <NavLink className="flex w-full" to={item.url}>
+          <NavLink className="flex w-full" end to={item.url}>
             {({ isActive }) => (
-              <SidebarMenuButton size="lg">
-                {isActive && (
-                  <div className="absolute inset-0 bg-gradient-to-r from-white/10 to-transparent opacity-20" />
-                )}
-                <div
-                  className={cn(
-                    'flex size-10 items-center justify-center rounded-full bg-accent',
-                    { 'bg-primary': isActive, 'bg-accent': !isActive }
-                  )}
-                >
-                  <item.icon
-                    className={cn(
-                      'size-5 shrink-0 transition-all duration-200',
-                      { 'text-white': isActive }
-                    )}
-                  />
-                </div>
-                <span className={cn('text-xl')}>{item.title}</span>
-              </SidebarMenuButton>
+              <NavSidebarMenuButton
+                icon={item.icon}
+                isActive={isActive}
+                title={item.title}
+              />
             )}
           </NavLink>
         </SidebarMenuItem>
