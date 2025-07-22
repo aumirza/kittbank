5e03077f563191c508981c432d99c0891b8e5d9c
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-22 20:25:44 +0530

    feat: implement Overview component with FinancialSnapshot, MonthlySpendingTrend, TicketsCard, TransactionsCard, and RecentTransactions

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,3 +1,31 @@
+import FinancialSnapshot from '@/components/overview/FinancialSnapshot';
+import { MonthlySpendingTrend } from '@/components/overview/MonthlySpendingTrend';
+import { RecentTransactions } from '@/components/overview/RecentTransactions';
+import { TicketsCard } from '@/components/overview/TicketsCard';
+import { TransactionsCard } from '@/components/overview/TransactionsCard';
+
 export default function Overview() {
-  return <div>Overview</div>;
+  return (
+    <div className="space-y-6">
+      {/*  */}
+      <FinancialSnapshot />
+
+      <div className="grid gap-6 md:grid-cols-4">
+        {/* Monthly Spending Trend Chart */}
+        <div className="md:col-span-3">
+          <MonthlySpendingTrend />
+        </div>
+
+        <TicketsCard />
+      </div>
+
+      <div className="grid gap-6 md:grid-cols-4">
+        <TransactionsCard />
+        <div className="md:col-span-3">
+          {/* Recent Transactions */}
+          <RecentTransactions />
+        </div>
+      </div>
+    </div>
+  );
 }
