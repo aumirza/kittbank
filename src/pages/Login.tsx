46f74ed4de5a481b3acb17182c83dc9bc4dc640b
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-22 16:41:26 +0530

    feat: add login illustration and update Login page with login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,17 @@
+import loginIllustration from '@/assets/images/login-illustration.png';
+import LoginForm from '@/components/LoginForm';
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-[#FEF6D5]">
+      <div className="flex w-full max-w-6xl items-center justify-between gap-10 ">
+        <div className="hidden md:block">
+          <img alt="Login Illustration" src={loginIllustration} />
+        </div>
+        <div className="w-full max-w-xl">
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+  );
 }
