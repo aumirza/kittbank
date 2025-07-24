c7fde75f404481c8d3bc934a4f533a3e2e3a4236
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-24 19:54:21 +0530

    feat: wrap setup page with page layout

--- src/pages/Setup.tsx
+++ src/pages/Setup.tsx
@@ -1,29 +1,32 @@
+import { PageLayout } from '@/components/PageLayout';
 import { ATMTab } from '@/components/setup/ATMTab';
 import { CurrencyTab } from '@/components/setup/CurrencyTab';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 export default function Setup() {
   return (
-    <Tabs className="w-full" defaultValue="atm">
-      <TabsList className="b flex h-11 space-x-2 shadow-none">
-        <TabsTrigger
-          className="h-10 rounded-full px-3 py-2 font-medium text-gray-700 text-sm hover:bg-gray-100 data-[state=active]:border data-[state=active]:border-primary data-[state=active]:bg-amber-100"
-          value="atm"
-        >
-          ATM
-        </TabsTrigger>
-        <TabsTrigger
-          className="h-10 rounded-full px-3 py-2 font-medium text-gray-700 text-sm hover:bg-gray-100 data-[state=active]:border data-[state=active]:border-primary data-[state=active]:bg-amber-100"
-          value="currency"
-        >
-          Currency
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent className="" value="atm">
-        <ATMTab />
-      </TabsContent>
-      <TabsContent className="p-4" value="currency">
-        <CurrencyTab />
-      </TabsContent>
-    </Tabs>
+    <PageLayout className="h-full" title="setup">
+      <Tabs className="h-full w-full" defaultValue="atm">
+        <TabsList className="b flex h-11 space-x-2 shadow-none">
+          <TabsTrigger
+            className="h-10 rounded-full px-3 py-2 font-medium text-gray-700 text-sm hover:bg-gray-100 data-[state=active]:border data-[state=active]:border-primary data-[state=active]:bg-amber-100"
+            value="atm"
+          >
+            ATM
+          </TabsTrigger>
+          <TabsTrigger
+            className="h-10 rounded-full px-3 py-2 font-medium text-gray-700 text-sm hover:bg-gray-100 data-[state=active]:border data-[state=active]:border-primary data-[state=active]:bg-amber-100"
+            value="currency"
+          >
+            Currency
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent className="" value="atm">
+          <ATMTab />
+        </TabsContent>
+        <TabsContent className="p-4" value="currency">
+          <CurrencyTab />
+        </TabsContent>
+      </Tabs>
+    </PageLayout>
   );
 }
