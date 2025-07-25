701cef54f8099b633606ce082c452d011e4a8cbb
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-25 16:01:40 +0530

    feat: implement token check and redirect in Login component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,13 @@
+import { Navigate } from 'react-router';
 import loginIllustration from '@/assets/images/login-illustration.png';
 import LoginForm from '@/components/LoginForm';
+import { useAuthStore } from '@/stores/authStore';
 
 export default function Login() {
+  const token = useAuthStore((state) => state.token);
+  if (token) {
+    return <Navigate replace to="/" />;
+  }
   return (
     <div className="flex h-screen w-full items-center justify-center bg-[#FEF6D5]">
       <div className="flex w-full max-w-6xl items-center justify-between gap-10 ">
